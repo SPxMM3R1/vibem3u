9b345973da756946d331516bfb330f38970054f4
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-08-09 14:36:26 -0400

    Enable continuous marquee for content titles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "cl.streambox.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.4.16"
+        versionCode = 22
+        versionName = "0.4.17"
     }
 
     buildTypes {
