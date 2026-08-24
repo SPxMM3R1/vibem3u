bc757e5ba433cb821c6970d700e2972c65667bc2
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-08-23 21:52:56 -0400

    Bump VibeM3U to 0.4.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "cl.streambox.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 32
-        versionName = "0.4.27"
+        versionCode = 33
+        versionName = "0.4.28"
     }
 
     buildTypes {
