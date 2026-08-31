dc2ba2b7f79c5b27faef52cfa6f430bf33f8016a
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-08-30 20:46:57 -0400

    Prepara version 0.4.57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "cl.streambox.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 61
-        versionName = "0.4.56"
+        versionCode = 62
+        versionName = "0.4.57"
         buildConfigField("boolean", "ENABLE_EXPERIMENTAL_VAVOO", "false")
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
