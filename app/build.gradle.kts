265abdc2659e08c71c3fae93775c5905929fd44a
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-03 19:44:19 -0400

    Publica VibeM3U 0.4.69

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "cl.streambox.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 73
-        versionName = "0.4.68"
+        versionCode = 74
+        versionName = "0.4.69"
         buildConfigField("boolean", "ENABLE_EXPERIMENTAL_VAVOO", "false")
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
