941abb5d5d70b01ba028cc0afde3e3d90925e846
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-06 19:06:38 -0300

    Bump version to 0.4.75

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 79
-        versionName = "0.4.74"
+        versionCode = 80
+        versionName = "0.4.75"
         buildConfigField("boolean", "ENABLE_EXPERIMENTAL_VAVOO", "false")
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
