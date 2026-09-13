34c504042613810a4c95d7a74214ad87d3522056
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-12 22:06:03 -0300

    Revert "Bump version to 0.4.82"
    This reverts commit c0796508ef3ffa2c9e6a5174a679ce9d817643f0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 87
-        versionName = "0.4.82"
+        versionCode = 86
+        versionName = "0.4.81"
         buildConfigField("boolean", "ENABLE_EXPERIMENTAL_VAVOO", "false")
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
