0022664efc17fa20b287e58e9ff0371b8d088c99
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-14 20:24:45 -0300

    Bump version to 0.4.90

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 94
-        versionName = "0.4.89"
+        versionCode = 95
+        versionName = "0.4.90"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
