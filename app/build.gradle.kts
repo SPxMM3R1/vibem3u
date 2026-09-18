4c2739c5628f71b5ab9a966c34ee7e7c95d16aef
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-17 22:33:12 -0300

    Reduce adaptive launcher mark size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 108
-        versionName = "0.5.02"
+        versionCode = 109
+        versionName = "0.5.03"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
