cadc399fd1c5cc32ab0b9d85d02e503daa9a3273
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-18 15:47:11 -0300

    chore: bump version to 0.5.04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 109
-        versionName = "0.5.03"
+        versionCode = 110
+        versionName = "0.5.04"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
