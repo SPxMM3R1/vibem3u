b3b4b7ec166d3b387499cea1e1a0cb9e9966dd8f
SPxMM3R1 <306895301+SPxMM3R1@users.noreply.github.com>
2026-09-23 19:36:07 -0300

    chore(release): bump VibeM3U to 0.5.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 120
-        versionName = "0.5.14"
+        versionCode = 121
+        versionName = "0.5.15"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
