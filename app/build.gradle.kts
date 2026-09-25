f9491cf3c17ceec2dc8acf050f5d94c10ad35fa3
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-25 19:31:15 -0300

    fix(settings): increase tab title size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 127
-        versionName = "0.5.21"
+        versionCode = 128
+        versionName = "0.5.22"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
