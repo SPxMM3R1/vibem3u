930f959d9977d6c6d47db7e6fe11201481e737d5
Roberto Ramos <roberto.ramos.dz@gmail.com>
2026-09-26 10:52:39 -0300

    chore(release): bump VibeM3U a 0.5.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         minSdk = 29
         targetSdk = 36
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 130
-        versionName = "0.5.24"
+        versionCode = 131
+        versionName = "0.5.25"
         buildConfigField("boolean", "ENABLE_APP_UPDATES", "true")
     }
 
